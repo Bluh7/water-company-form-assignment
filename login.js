274758cc35eb64828097167d4ef99b897b67bd8c
Bluh7 <gabrielperazzob@proton.me>
2025-11-08 20:48:52 -0300

    simple js added

---
+++ login.js
@@ -0,0 +1,34 @@
+const userNameInput = document.querySelector('[name="usuario"]');
+const userPassInput = document.querySelector('[name="usuario_senha"]');
+
+const defaultUser = {
+    username: "admin",
+    password: "admin",
+    token: "e40da6f4-4a5e-4a8f-8156-360393242a8e"
+};
+
+const loginForm = document.getElementById("login-form");
+
+loginForm.addEventListener("submit", async e => {
+    const userName = userNameInput.value;
+    const userPass = userPassInput.value;
+
+    if (!userName || !userPass) {
+        e.preventDefault();
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+
+    if (userName !== defaultUser.username || userPass !== defaultUser.password) {
+        e.preventDefault();
+        alert("O usuário ou senha estão errados.");
+        return;
+    }
+
+    if (userName === defaultUser.username && userPass === defaultUser.password) {
+        e.preventDefault();
+        await cookieStore.set({ name: "user", value: defaultUser.token, path: "/" });
+        window.location.href = "/";
+    }
+
+});
---
+++ main.js
@@ -0,0 +1,89 @@
+const users = ["e40da6f4-4a5e-4a8f-8156-360393242a8e"];
+
+const checkUser = async () => {
+    const user = await cookieStore.get("user");
+    const userToken = user?.value;
+    
+    if (!users.includes(userToken) || !(userToken)) {
+        await cookieStore.delete("user");
+        window.location.href = "/login.html";
+        return;
+    }
+}
+
+checkUser();
+
+const productCodeInput           = document.querySelector('[name="produto_codigo"]');
+const productNameInput           = document.querySelector('[name="produto_nome"]');
+const productPriceInput          = document.querySelector('[name="produto_preco"]');
+const productQuantityInput       = document.querySelector('[name="produto_quantidade"]');
+const productCategorySelect      = document.querySelector('[name="produto_categoria"]');
+const productManufacturerSelect  = document.querySelector('[name="produto_fabricante"]');
+const productDescriptionTextarea = document.querySelector('[name="produto_descricao"]');
+
+const productForm = document.getElementById("produto-form");
+
+const productCategories    = ["galao", "torneira_registro", "bomba_pressurizador", "filtro_purificador", "torre_bebedouro"];
+const productManufacturers = ["brita", "electrolux", "tigre", "lorenzetti", "igua"];
+
+productCodeInput.addEventListener("input", () => {
+    productCodeInput.value = productCodeInput.value.replace(/[^0-9]/g, "");
+});
+
+productNameInput.addEventListener("input", () => {
+    productNameInput.value = productNameInput.value.replace(/[^A-Za-zÀ-ÖØ-öø-ÿ0-9 ]/g, "");
+    productNameInput.value = productNameInput.value.replace(/ +$/, "");
+});
+
+productPriceInput.addEventListener("input", () => {
+  let v = productPriceInput.value;
+  v = v.replace(/[^0-9.]/g, "");
+
+  const parts = v.split(".");
+  if (parts.length > 2) {
+    v = parts.shift() + "." + parts.join("");
+  }
+
+  if (v.includes(".")) {
+    const [inteiro, dec] = v.split(".");
+    v = inteiro + "." + (dec ? dec.slice(0, 2) : "");
+  }
+
+  if (v.startsWith(".")) v = "0" + v;
+
+  productPriceInput.value = v;
+});
+
+productQuantityInput.addEventListener("input", () => {
+    productQuantityInput.value = productQuantityInput.value.replace(/[^0-9]/g, "")
+    productQuantityInput.value = productQuantityInput.value.replace(/^0+(?!$)/, "")
+});
+
+productForm.addEventListener("submit", e => {
+    const productCode        = productCodeInput.value;
+    const productName        = productNameInput.value;
+    const productPrice       = productPriceInput.value;
+    const productQuantity    = productQuantityInput.value;
+    const productDescription = productDescriptionTextarea.value;
+
+    if (!productCode || !productName || !productPrice || !productQuantity || !productDescription) {
+        e.preventDefault();
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+
+    const productCategory     = productCategorySelect.value;
+    const productManufacturer = productManufacturerSelect.value;
+
+    if (!productCategory || !productManufacturer) {
+        e.preventDefault();
+        alert("Por favor, selecione uma categoria e fabricante.");
+        return;
+    }
+
+    if (!productCategories.includes(productCategory) || !productManufacturers.includes(productManufacturer)) {
+        e.preventDefault();
+        alert("Por favor, selecione um fabricante e categoria válidos.");
+        return;
+    }
+});
